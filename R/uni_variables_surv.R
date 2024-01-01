57c1e7454b824211d4c801e1312a1691fb03f6fa
Maryam Mahootiha <53992619+mahootiha-maryam@users.noreply.github.com>
2024-01-01 12:50:40 -0500

    Update

--- R/uni_variables_surv.R
+++ R/uni_variables_surv.R
@@ -2,7 +2,6 @@ library(survival)
 library(rms)
 
 data_bch <- read.csv("C:/kannlab/R/braf_combined_labeled.csv")
-# data_bch <- read.csv("C:/kannlab/R/cbtn_have_braf.csv")
 
 data_bch$time_event <- data_bch$time_event / 30.44
 
@@ -16,10 +15,7 @@ braf_bch$braf <- ifelse(braf_bch$braf == 0, "V600E",
                         ifelse(braf_bch$braf == 1, "Fusion",
                                ifelse(braf_bch$braf == 2, "Wild", "unknown")))
 
-
 braf_bch$braf <- factor(braf_bch$braf, levels = c("V600E", "Fusion", "Wild"))
-
-
 
 phmodel <- cph(Surv(time_event, event) ~ braf , 
                data=braf_bch)
@@ -31,12 +27,10 @@ km_fit <- survfit(Surv(time_event, event) ~ braf, data = braf_bch)
 # Plot the Kaplan-Meier curves
 plot(km_fit, col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2, xlab = "Time (months)", ylab = "Recurrence-free survival")
 legend("topright", legend = levels(braf_bch$braf), col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2)
-
 
 ##################################################################################
 
 data_bch <- read.csv("C:/kannlab/R/braf_combined_total.csv")
-# data_bch <- read.csv("C:/kannlab/R/cbtn_have_braf.csv")
 
 data_bch$time_event <- data_bch$time_event / 30.44
 
@@ -53,8 +47,6 @@ braf_bch$braf <- ifelse(braf_bch$braf == 0, "V600E",
 
 braf_bch$braf <- factor(braf_bch$braf, levels = c("V600E", "Fusion", "Wild"))
 
-
-
 phmodel <- cph(Surv(time_event, event) ~ braf , 
                data=braf_bch)
 
@@ -65,13 +57,3 @@ km_fit <- survfit(Surv(time_event, event) ~ braf, data = braf_bch)
 # Plot the Kaplan-Meier curves
 plot(km_fit, col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2, xlab = "Time (months)", ylab = "Recurrence-free survival")
 legend("topright", legend = levels(braf_bch$braf), col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2)
-
-
-
-
-# dd = datadist(braf_bch)
-# options(datadist='dd')
-# nom <- nomogram(phmodel, fun = function(x) 1- plogis(x),
-#               funlabel='3 year survival rate' )
-# 
-# plot(nom)
