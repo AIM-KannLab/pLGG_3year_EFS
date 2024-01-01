c3f7123830c0bf413800648e8127cd1b4c7ab69c
Maryam Mahootiha <53992619+mahootiha-maryam@users.noreply.github.com>
2024-01-01 12:49:08 -0500

    Add files via upload

---
+++ uni_variables_surv.R
@@ -0,0 +1,77 @@
+library(survival)
+library(rms)
+
+data_bch <- read.csv("C:/kannlab/R/braf_combined_labeled.csv")
+# data_bch <- read.csv("C:/kannlab/R/cbtn_have_braf.csv")
+
+data_bch$time_event <- data_bch$time_event / 30.44
+
+braf_bch <- data.frame(
+  braf = data_bch$braf,
+  time_event = data_bch$time_event,
+  event = data_bch$event
+)
+
+braf_bch$braf <- ifelse(braf_bch$braf == 0, "V600E",
+                        ifelse(braf_bch$braf == 1, "Fusion",
+                               ifelse(braf_bch$braf == 2, "Wild", "unknown")))
+
+
+braf_bch$braf <- factor(braf_bch$braf, levels = c("V600E", "Fusion", "Wild"))
+
+
+
+phmodel <- cph(Surv(time_event, event) ~ braf , 
+               data=braf_bch)
+
+survdiff(Surv(time_event, event) ~ braf, data = braf_bch)
+
+km_fit <- survfit(Surv(time_event, event) ~ braf, data = braf_bch)
+
+# Plot the Kaplan-Meier curves
+plot(km_fit, col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2, xlab = "Time (months)", ylab = "Recurrence-free survival")
+legend("topright", legend = levels(braf_bch$braf), col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2)
+
+
+##################################################################################
+
+data_bch <- read.csv("C:/kannlab/R/braf_combined_total.csv")
+# data_bch <- read.csv("C:/kannlab/R/cbtn_have_braf.csv")
+
+data_bch$time_event <- data_bch$time_event / 30.44
+
+braf_bch <- data.frame(
+  braf = data_bch$braf,
+  time_event = data_bch$time_event,
+  event = data_bch$event
+)
+
+braf_bch$braf <- ifelse(braf_bch$braf == 0, "V600E",
+                        ifelse(braf_bch$braf == 1, "Fusion",
+                               ifelse(braf_bch$braf == 2, "Wild", "unknown")))
+
+
+braf_bch$braf <- factor(braf_bch$braf, levels = c("V600E", "Fusion", "Wild"))
+
+
+
+phmodel <- cph(Surv(time_event, event) ~ braf , 
+               data=braf_bch)
+
+survdiff(Surv(time_event, event) ~ braf, data = braf_bch)
+
+km_fit <- survfit(Surv(time_event, event) ~ braf, data = braf_bch)
+
+# Plot the Kaplan-Meier curves
+plot(km_fit, col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2, xlab = "Time (months)", ylab = "Recurrence-free survival")
+legend("topright", legend = levels(braf_bch$braf), col = c("brown2", "seagreen", "royalblue"), lty = 1, lwd = 2)
+
+
+
+
+# dd = datadist(braf_bch)
+# options(datadist='dd')
+# nom <- nomogram(phmodel, fun = function(x) 1- plogis(x),
+#               funlabel='3 year survival rate' )
+# 
+# plot(nom)
